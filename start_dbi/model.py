4b46e6d76f03fd192cacc3b256da7d6f78fc5aaf
ChrisTimperley <christimperley@googlemail.com>
2018-08-15 10:31:44 -0400

    removed comments from obvious methods

--- start_dbi/model.py
+++ start_dbi/model.py
@@ -8,9 +8,6 @@ class Model(object):
     # type: (str) -> Model
     @staticmethod
     def from_file(filename):
-        """
-        Loads a precomputed model from a given file.
-        """
         # NOTE this may require mode 'rb' if the file is in a binary format
         with open(filename, 'r') as f:
             # TODO unpack
@@ -18,7 +15,4 @@ class Model(object):
 
     # type: (str) -> None
     def to_file(filename):
-        """
-        Saves this model to a given file.
-        """
         raise NotImplementedError
