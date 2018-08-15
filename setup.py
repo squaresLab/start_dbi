586eec0e79d528aa925e03c39bf462dbabaf2f19
ChrisTimperley <christimperley@googlemail.com>
2018-08-15 09:54:34 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import glob
+import setuptools
+
+setuptools.setup(
+    name='start_dbi',
+    version='0.0.1',
+    description='Provides the DBI module for START.',
+    author='Deby Katz; Chris Timperley'
+    author_email='dskatz@gmail.com; christimperley@gmail.com',
+    url='https://github.com/squaresLab/start-dbi',
+    install_requires=['start_core'],
+    packages=['start_dbi']
+)
