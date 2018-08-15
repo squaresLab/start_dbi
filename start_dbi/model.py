8457c10134817c5fcfd15ea3d8c80e07d39e0591
ChrisTimperley <christimperley@googlemail.com>
2018-08-15 10:17:20 -0400

    added Model data structure

---
+++ start_dbi/model.py
@@ -0,0 +1,18 @@
+class Model(object):
+    # type: (str) -> Model
+    @staticmethod
+    def from_file(filename):
+        """
+        Loads a precomputed model from a given file.
+        """
+        # NOTE this may require mode 'rb' if the file is in a binary format
+        with open(filename, 'r') as f:
+            # TODO unpack
+            raise NotImplementedError
+
+    # type: (str) -> None
+    def to_file(filename):
+        """
+        Saves this model to a given file.
+        """
+        raise NotImplementedError
