6c6718cf798bbc7da10bc2d2392cb1d07c58c942
ChrisTimperley <christimperley@googlemail.com>
2018-08-15 10:25:53 -0400

    renamed build_model to learn

--- start_dbi/__init__.py
+++ start_dbi/__init__.py
@@ -3,5 +3,14 @@ This package provides an interface to START's DBI module.
 """
 import logging
 
+from start_core.scenario import Scenario
+
+from .model import Model
+
 logger = logging.getLogger(__name__)  # type: logging.Logger
 logger.setLevel(logging.DEBUG)
+
+
+# type: (Scenario) -> Model
+def learn(scenario):
+    raise NotImplementedError
